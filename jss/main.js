65962b9bbc63538c353f51a0fd0d4c3fcb533d87
CarloCassini <c.cassini87@gmail.com>
2023-07-20 16:29:04 +0200

    fix getting value

--- jss/main.js
+++ jss/main.js
@@ -1,1 +1,20 @@
 console.log("js ok");
+
+// dichiarazione delle variabili
+let userKm;
+let userEta;
+const MyButton = document.getElementById("send-button");
+let prezzoViaggio;
+
+MyButton.addEventListener("click", function () {
+  userKm = parseFloat(document.getElementById("user-km").value);
+  userEta = parseInt(document.getElementById("user-eta").value);
+
+  console.log(userKm);
+  console.log(userEta);
+});
+
+// vecchio codice
+prezzoViaggio = userKm * 0.21;
+
+console.log(prezzoViaggio);
